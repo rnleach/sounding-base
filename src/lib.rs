98204dc9911ad6095cef6d862d08849230134fb3
Ryan Leach <ryan.n.leach@gmail.com>
2017-11-10 00:13:45 -0700

    Moved validation to its own crate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
 #![warn(missing_docs)]
-#![recursion_limit = "1024"]
 //! Library to represent an atmospheric sounding.
 
 extern crate chrono;
-
-#[macro_use]
-extern crate error_chain;
-
-pub mod error;
-pub use error::*;
 
 pub mod missing_value;
 pub use missing_value::{OptionVal, MissingData};
