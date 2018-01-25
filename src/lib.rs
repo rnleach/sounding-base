2036290c3cdbe808f48549dbfeecc8badca45303
rnleach <rnleach@users.noreply.github.com>
2018-01-24 20:17:48 -0700

    Added SMALL_VEC_SIZE const for derived crates to use.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,11 @@
 //! Library to represent an atmospheric sounding.
 
 extern crate chrono;
+extern crate smallvec;
 
 pub mod sounding;
 pub use sounding::{DataRow, Index, Profile, Sounding, Surface};
+
+/// The size used in `smallvec` for the number of items to place on the stack. This is mainly used
+/// in derived crates.
+pub const SMALL_VEC_SIZE: usize = 3;
