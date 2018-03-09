b9951315b32623d7f1541100408b2322ee43f826
Ryan <rnleach@users.noreply.github.com>
2018-03-08 18:56:01 -0700

    Change to use cargo readme for generating readme file. Progress on #3.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
 #![deny(missing_docs)]
-//! Library to represent an atmospheric sounding with pressure as the vertical coordinate.
+
+/*!
+
+Library to represent an atmospheric sounding with pressure as the vertical coordinate.
+Data formats and algorithms for atmospheric soundings. The base crate is meant to be a common 
+base  for other crates to build on. These crates may be for managing a data-store, displaying
+data, or saving and loading files.
+
+The emphasis of this crate is data representation and a common type for systems using sounding
+data to build on and use.
+
+*/
 
 //
 // API
