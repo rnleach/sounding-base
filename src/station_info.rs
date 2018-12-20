5e98dd709a6b11e8bd489fc625053f0cad280519
Ryan <rnleach@users.noreply.github.com>
2018-12-19 20:21:51 -0700

    Renamed some methods for consistency and deprecated old names. Added documentation.

--- src/station_info.rs
+++ src/station_info.rs
@@ -33,6 +33,14 @@ impl StationInfo {
     /// let _stn = StationInfo::new_with_values(12345, (45.2,-113.5), Feet(2000.0));
     /// let _stn = StationInfo::new_with_values(12345, (45.2,-113.5), some(Meters(2000.0)));
     /// let _stn = StationInfo::new_with_values(12345, (45.2,-113.5), some(Feet(2000.0)));
+    /// let _stn = StationInfo::new_with_values(12345, Some((45.2,-113.5)), Meters(2000.0));
+    /// let _stn = StationInfo::new_with_values(12345, Some((45.2,-113.5)), Feet(2000.0));
+    /// let _stn = StationInfo::new_with_values(12345, Some((45.2,-113.5)), some(Meters(2000.0)));
+    /// let _stn = StationInfo::new_with_values(12345, Some((45.2,-113.5)), some(Feet(2000.0)));
+    /// let _stn = StationInfo::new_with_values(Some(12345), None, Meters(2000.0));
+    /// let _stn = StationInfo::new_with_values(Some(12345), None, Feet(2000.0));
+    /// let _stn = StationInfo::new_with_values(None, (45.2,-113.5), some(Meters(2000.0)));
+    /// let _stn = StationInfo::new_with_values(None, (45.2,-113.5), some(Feet(2000.0)));
     ///
     /// // Note that lat-lon is an `Option` and not an `Optioned`
     /// let _stn = StationInfo::new_with_values(some(12345), None, none::<Feet>());
@@ -75,6 +83,16 @@ impl StationInfo {
     }
 
     /// Builder method to add a station number.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use sounding_base::StationInfo;
+    ///
+    /// assert_eq!(StationInfo::new().with_station(12345).station_num().unwrap(), 12345);
+    /// assert_eq!(StationInfo::new().with_station(Some(12345)).station_num().unwrap(), 12345);
+    ///
+    /// ```
     #[inline]
     pub fn with_station<T>(mut self, number: T) -> Self
     where
@@ -86,6 +104,21 @@ impl StationInfo {
     }
 
     /// Builder method to add a location.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use sounding_base::StationInfo;
+    ///
+    /// assert_eq!(
+    ///     StationInfo::new().with_lat_lon((45.0, -116.0)).location().unwrap(), (45.0, -116.0));
+    /// assert_eq!(
+    ///     StationInfo::new().with_lat_lon(Some((45.0, -116.0)))
+    ///         .location()
+    ///         .unwrap(),
+    ///     (45.0, -116.0));
+    ///
+    /// ```
     #[inline]
     pub fn with_lat_lon<T>(mut self, coords: T) -> Self
     where
@@ -96,12 +129,34 @@ impl StationInfo {
     }
 
     /// Builder method to add elevation.
+    ///
+    /// # Examples
+    ///```rust
+    /// use metfor::{Meters, Feet, Km};
+    /// use sounding_base::StationInfo;
+    /// use optional::{some, none};
+    ///
+    /// let _info = StationInfo::new().with_elevation(Feet(200.0));
+    /// let _info = StationInfo::new().with_elevation(Meters(200.0));
+    /// let _info = StationInfo::new().with_elevation(Km(2.0));
+    /// let _info = StationInfo::new().with_elevation(some(Feet(200.0)));
+    /// let _info = StationInfo::new().with_elevation(some(Meters(200.0)));
+    /// let _info = StationInfo::new().with_elevation(some(Km(2.0)));
+    /// let _info = StationInfo::new().with_elevation(none::<Feet>());
+    /// let _info = StationInfo::new().with_elevation(none::<Meters>());
+    /// let _info = StationInfo::new().with_elevation(none::<Km>());
+    ///```
     #[inline]
-    pub fn with_elevation<T>(mut self, elev: T) -> Self
+    pub fn with_elevation<T, U>(mut self, elev: T) -> Self
     where
-        Optioned<Meters>: From<T>,
+        Optioned<U>: From<T>,
+        U: optional::Noned + metfor::Length,
+        Meters: From<U>,
     {
-        self.elevation = Optioned::from(elev);
+        let elevation: Optioned<U> = Optioned::from(elev);
+        let elevation: Optioned<Meters> = elevation.map_t(Meters::from);
+
+        self.elevation = elevation;
         self
     }
 
